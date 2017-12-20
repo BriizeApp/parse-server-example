3a0beaa3b4a282a891f2490a1b0c079e952a141b
BriizeApp <30712282+BriizeApp@users.noreply.github.com>
2017-12-20 02:37:15 -0800

    new index

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_9mt0zg1v:vp2t04945ds1rb6cv8pt3ravd4@ds121674.mlab.com:21674/heroku_9mt0zg1v',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'bri009',
+  masterKey: process.env.MASTER_KEY || '009bri', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://briize.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
