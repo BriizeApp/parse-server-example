5735e7ef049148d5a3f05d46d4c3afdecf9234ef
BriizeApp <30712282+BriizeApp@users.noreply.github.com>
2019-02-10 12:34:38 -0800

    a1a
    a1

--- cloud/main.js
+++ cloud/main.js
@@ -11,14 +11,14 @@
     var someKey = request.someKey;
     var data = params.data;
 
-//     // Set our query to target specific user 
-//     var recipientUser = new Parse.Query(Parse.User);
-//     recipientUser.equalTo("objectId", request.params.someKey);
+    // Set our query to target specific user 
+    var targetUser = new Parse.Query(Parse.User);
+    targetUser.equalTo("objectId", request.params.someKey);
 
     // Set our installation query
     var pushQuery = new Parse.Query(Parse.Installation);
     pushQuery.equalTo('deviceType', 'ios');
-//     pushQuery.equalTo('user', recipientUser);
+    pushQuery.matchesQuery('targetUser', targetUser);
     pushQuery.equalTo('userId', request.params.someKey); 
 
     // Send push notification to query
