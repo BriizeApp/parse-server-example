a18573effd3bbaf09c2e687bceb4dda5dd69b387
BriizeApp <30712282+BriizeApp@users.noreply.github.com>
2019-02-10 02:12:02 -0800

    update
    update

--- cloud/main.js
+++ cloud/main.js
@@ -4,41 +4,42 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define("requestExpert", async request=> {
-//   console.log("Inside requestExpert");
-//   var params = request.params;
-//   var user = request.user;
-//   var sessionToken = user.getSessionToken();
-//   var someKey = request.someKey;
-//   var data = params.data;
+  console.log("Inside requestExpert");
+  var params = request.params;
+  var user = request.user;
+  var sessionToken = user.getSessionToken();
+  var someKey = request.someKey;
+  var data = params.data;
 
-//  // Set our query to target specific user 
-//   var recipientUser = new Parse.Query(Parse.User);
-//   recipientUser.equalTo("objectId", request.params.someKey);
+ // Set our query to target specific user 
+  var recipientUser = new Parse.Query(Parse.User);
+  recipientUser.equalTo("objectId", request.params.someKey);
 
-// // Set our installation query
-//    const pushQuery = new Parse.Query(Parse.Installation);
-//    pushQuery.equalTo('deviceType', 'ios');
-//    pushQuery.matchesQuery('user', recipientUser); 
+// Set our installation query
+   const pushQuery = new Parse.Query(Parse.Installation);
+   pushQuery.equalTo('deviceType', 'ios');
+   pushQuery.matchesQuery('user', recipientUser); 
 
-//    let results;
-//     try {
-//         results = await pushQuery.find();
-//         console.log("pushQuery got " + results.length);
-//        }
-//     } catch(error){
-//         throw error.message;
-//     }
+   let results;
+    try {
+        results = await pushQuery.find();
+        console.log("pushQuery got " + results.length);
+      
+    } catch(error){
+        throw error.message;
+    }
   
-//   // Send push notification to query
-//   Parse.Push.send({
-//     where: pushQuery,
-//     data: {
-//         alert: 'One more test 1',
-//         badge: 1,
-//         sound: 'default',
-//         objectId: someKey,
-//         'content-available': 1
-//     }
-// }, { useMasterKey: true });
+  // Send push notification to query
+  Parse.Push.send({
+    where: pushQuery,
+    data: {
+        alert: 'One more test 1',
+        badge: 1,
+        sound: 'default',
+        objectId: someKey,
+        'content-available': 1
+    }
+}, { useMasterKey: true });
+
 });
 
